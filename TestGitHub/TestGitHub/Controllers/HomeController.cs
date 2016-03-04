6ab07dbe54f123738850704de0db7716bfd30279
Nelavoy <chanduegc@gmail.com>
2016-03-04 15:56:28 +0530

    Controller Modified

--- TestGitHub/TestGitHub/Controllers/HomeController.cs
+++ TestGitHub/TestGitHub/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace TestGitHub.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page 3";
 
             return View();
         }
